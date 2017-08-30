9c1efaf1606fe49462847c38e57e9b124d616c4a
나비 <nabigraphics@iMac.local>
2017-08-30 16:11:54 +0900

    popover 컴포넌트 추가

---
+++ src/components/Popover.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+class Popover extends Component {
+    constructor(props){
+        super(props);
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+        this.handleResizePopover = this.handleResizePopover.bind(this);
+        this.popOpen = this.popOpen.bind(this);
+        this.state = {
+            active:false,
+            top:0,
+            left:0,
+            translateX:0,
+            translateY:0
+        }
+        this.popover = null;
+    }
+    handleResizePopover(){
+        this.popOpen();
+    }
+    handleOutsideClick(e){
+        if(!this.state.active){
+            return;
+        }
+        const check = ReactDOM.findDOMNode(this);
+        if(check.contains(e.target) || e.target == this.props.target){
+            return;
+        }
+        e.stopPropagation();
+        this.props.onClose();
+    }
+    popOpen(){
+        if(window.innerWidth <= 575){
+            this.setState({
+                translateX:0,
+                translateY:0
+            })
+            return;
+        }
+        const bt_rect = this.props.target.getBoundingClientRect();
+        const pop_rect = this.popover.getBoundingClientRect();
+        this.setState({
+            translateX:(bt_rect.left - pop_rect.width/2 - pop_rect.width/2 + bt_rect.width/2 + 20 ),
+            translateY:(bt_rect.top + bt_rect.height/1.5)
+        })
+    }
+    componentWillUnmount(){
+        window.removeEventListener("resize", this.handleResizePopover);
+        document.removeEventListener("mouseup",this.handleOutsideClick);
+        document.removeEventListener("touchstart",this.handleOutsideClick);
+    }
+    componentDidUpdate(prevProps,prevState){
+        if(this.props.isOpen){
+            if(prevProps.isOpen !== this.props.isOpen){
+                this.popOpen();
+            }
+        }
+    }
+    componentWillReceiveProps(props){
+        if(props.isOpen){
+            this.setState({active:true});
+            window.addEventListener("resize", this.handleResizePopover);
+            document.addEventListener("mouseup",this.handleOutsideClick);
+            document.addEventListener("touchstart",this.handleOutsideClick);
+        }else{
+            this.setState({active:false});
+            window.removeEventListener("resize", this.handleResizePopover);
+            document.removeEventListener("mouseup",this.handleOutsideClick);
+            document.removeEventListener("touchstart",this.handleOutsideClick);
+        }
+    }
+    render() {
+        const style = {
+            top:this.state.top,
+            left:this.state.left,
+            transform:`translate(${this.state.translateX}px, ${this.state.translateY}px)`
+        }
+        if(this.props.isOpen){
+            return (
+                <ul
+                ref={ref => this.popover = ref}
+                style={style}
+                className={ (this.props.className) + " " + (this.props.isOpen ? "active" : "hidden")}
+                >
+                {this.props.children}
+                </ul>
+            )
+        }else{
+            return null
+        }
+    }
+}
+
+export default Popover;
