98edf9be780001303133569be1d83b5e8f652b2d
나비 <hbh2692@naver.com>
2017-09-03 22:12:08 +0900

    adminpage route setting.

--- src/route/Adminpage.js
+++ src/route/Adminpage.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Header from '../containers/Header';
+import FileUpload from '../components/FileUpload';
 import Content from '../containers/Content';
 class Adminpage extends Component {
     render() {
         return (
-            <div>
-                <Header/>
+            <FileUpload className="app-innerlayout">
                 <Content/>
-            </div>
+            </FileUpload>
         );
     }
 }
