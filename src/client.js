fe74b7ed35750608661ca0f77c93c0b5153ac161
나비 <hbh2692@naver.com>
2017-09-03 22:13:07 +0900

    redux devtool 코드 추가

--- src/client.js
+++ src/client.js
@@ -1,12 +1,13 @@
 import App from './app';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import * as actions from './actions';
 import {Provider} from 'react-redux';
 
-const store = createStore(reducers,applyMiddleware(thunk));
+const store = createStore(reducers,compose(applyMiddleware(thunk),devToolsEnhancer()));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('app'));
