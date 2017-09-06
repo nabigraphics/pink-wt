39551e81c93be6cef3e1b8c56ffc49bc03ff4a4d
나비 <hbh2692@naver.com>
2017-09-07 03:26:38 +0900

    react Masonry component 적용 및 contents 요청 추가.

---
+++ src/components/Masonryitem.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import FileIcon from './FileIcon';
+import Checkbox from './Checkbox';
+import Popover from './Popover';
+const Menuoption = (props) => {
+    return (
+        <div className="item-menu">
+            <Checkbox checked={props.checkboxchkd} target={"checkboxcomponent_" + props.id} className="checkbox" onClick={props.onClick} />
+        </div>
+    )
+}
+class Masonryitem extends Component {
+    constructor(props){
+        super(props);
+        this.itemClick = this.itemClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.popovertoggle = this.popovertoggle.bind(this);
+        this.state = {
+            checked:"",
+            checkedbool:false,
+            more_popover:false
+        }
+        this.MorePopover = null;
+    }
+    itemClick(type,hash) {
+        
+    }
+    popovertoggle(target){
+        switch(target){
+            case "more":
+                this.setState({
+                    more_popover: !this.state.more_popover
+                });
+            break;
+        }
+    }
+    handleClick(){
+        if(this.state.checked == ""){
+            this.setState({
+                checked:"checked",
+                checkedbool:true
+            });
+        }else{
+            this.setState({
+                checked:"",
+                checkedbool:false
+            });
+        }
+    }
+    render() {
+        const type = this.props.filetype.split('/')[0];
+        const hash = this.props.hash;
+        const thumb = this.props.thumb;
+        return (
+            <div className={"item " + this.state.checked + " item-shadow-box"}>
+                <div className="item-" className="item-icon">
+                    <Menuoption id={this.props.id} checkboxchkd={this.state.checkedbool} onClick={this.handleClick} />
+                    <FileIcon onClick={this.handleClick} icon={type} thumb={thumb ? thumb : false} />
+                </div>
+                <div className="item-content">
+                    <div>
+                        <h5>{this.props.filename}</h5>
+                    </div>
+                    <hr/>
+                    <div className="item-menu-bt">
+                        <div className="item-menu-bt-left">
+                            <i className="material-icons">file_download</i>
+                            <i className="material-icons">share</i>
+                        </div>
+                        <div className="item-menu-bt-right">
+                            <i ref={ref => this.MorePopover = ref} onClick={() => this.popovertoggle("more")} className="material-icons">more_horiz</i>
+                        </div>
+                        <Popover position="bottom-right" onClose={() => this.popovertoggle("more") } target={this.MorePopover} className="popover" isOpen={this.state.more_popover}>
+                            <li>이름 변경</li>
+                            <li>삭제</li>
+                        </Popover>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Masonryitem);
--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,13 +1,106 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Masonry from 'react-masonry-component';
+import Masonryitem from '../components/Masonryitem';
+import axios from 'axios';
 class Content extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.MasonryResize = this.MasonryResize.bind(this);
+        this.handleLayoutComplete = this.handleLayoutComplete.bind(this);
+        this.masonry = null;
+        this.state = {
+            checked:""
+        }
+    }
+    handleClick() {
+        if(this.state.checked == "checked"){
+            this.setState({
+                checked:""
+            })
+        }else{
+            // this.setState({
+            //     checked:"checked"
+            // })
+        }
+    }
+    MasonryResize(){
+        if(window.innerWidth <= 755 && window.innerWidth > 412){
+            this.masonry.options.columnWidth = 180
+
+        }else if(window.innerWidth <= 412 && window.innerWidth > 320){
+            this.masonry.options.columnWidth = 170
+
+        }else if(window.innerWidth <= 320){
+            this.masonry.options.columnWidth = 140
+
+        }else{
+            this.masonry.options.columnWidth = 240
+        }
+    }
+    handleLayoutComplete(dd){
+        // console.log(dd);
+    }
+    componentDidMount(){
+        // this.masonry.on('layoutComplete', this.handleLayoutComplete);
+        // this.masonry.hide();
+        // console.log(this.masonry);
+        this.MasonryResize();
+        window.addEventListener("resize", this.MasonryResize);
+        axios.get('/contents').then((res) => {
+            this.props.contentload(res.data);
+        })
+    }
+    componentWillUnmount(){
+        window.removeEventListener("resize", this.MasonryResize);
+    }
     render() {
+        const options = {
+            columnWidth: 240,
+            itemSelector: '.item',
+            gutter: 20,
+            isFitWidth: true
+        }
         return (
-            <div>
-                Content
+            
+            <div className="content">
+                <Masonry
+                ref={ref => this.masonry = this.masonry || ref.masonry}
+                className={'content-grid'}
+                options={options}
+                enableResizableChildren
+                updateOnEachImageLoad
+                >
+                {this.props.content.map((content,i) => { return(
+                    <Masonryitem 
+                    checked={this.state.checked} 
+                    onChecked={this.handleClick} 
+                    id={i}
+                    key={content.hash} 
+                    filename={content.filename} 
+                    filetype={content.filetype} 
+                    hash={content.hash} 
+                    thumb={content.thumb} 
+                    url={content.url} />
+                )})}
+                </Masonry>
             </div>
         );
     }
 }
 
-export default Content;
+const mapStateToProps = (state) => {
+    // console.log(typeof(state.files.get('content2')));
+    return{
+        content:state.files.get('content')
+    }
+}
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        contentload:(file) => {dispatch(actions.ContentsManager('load',file))}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Content);
