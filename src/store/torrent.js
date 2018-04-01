dcd688512ebbda47bb34c0c1bbe4ed2bdea958b6
graphics Nabi <hbh2692@naver.com>
2018-04-02 03:50:40 +0900

    torrent store refactoring.

--- src/store/torrent.js
+++ src/store/torrent.js
@@ -1,8 +1,10 @@
-import { observable, toJS, action } from 'mobx';
+import { observable, action } from 'mobx';
 import WebTorrent from 'webtorrent';
 import io from 'socket.io-client';
 import axios from 'axios';
-
+import shortid from 'shortid';
+import map from 'async/map';
+import config from '../../server/config';
 const TRACKERS = [
     ["http://tracker.moe.cloud/announce"],
     ["udp://tracker.moe.cloud/"],
@@ -10,10 +12,19 @@ const TRACKERS = [
 ]
 export default class TorrentStore {
 
-    @observable torrents = [];
-    @observable updateQueue = {};
+    @observable torrents = []
+    @observable updateQueue = {}
     @observable torrentInfo
-    @observable currentTorrent
+    @observable currentTorrent = {
+        isProgress: false,
+        finished: false,
+        progress: 0,
+        downloaded: 0,
+        uploaded: 0,
+        downloadSpeed: 0,
+        uploadSpeed: 0,
+    }
+    @observable currentTorrentFiles = []
 
     constructor() {
         this.initialize();
@@ -63,6 +74,7 @@ export default class TorrentStore {
             progress,
             numPeers
         } = torrent;
+
         this.currentTorrent = {
             ...this.currentTorrent,
             name,
@@ -75,8 +87,50 @@ export default class TorrentStore {
             downloadSpeed,
             uploadSpeed,
             progress,
-            numPeers
+            numPeers,
         }
+        // map(torrent.files, (file, callback) => {
+        //     // console.log(file);
+        //     let data = {
+        //         done: file.done,
+        //         key: shortid.generate(),
+        //         name: file.name,
+        //         length: file.length,
+        //         progress: file.progress,
+        //     }
+        //     callback(null, data);
+        //     // if (file.done) {
+        //     //     file.getBlob((err, blob) => {
+        //     //         data.blob = blob;
+        //     //         file.getBlobURL((err, url) => {
+        //     //             data.url = url;
+        //     //             callback(null, data);
+        //     //         })
+        //     //     })
+        //     // } else {
+        //     //     callback(null, data);
+        //     // }
+        // }, (err, results) => {
+        //     if (err) return console.log(err);
+        //     // console.log(results);
+        //     // this.currentTorrent.files = results;
+        //     this.currentTorrent = {
+        //         ...this.currentTorrent,
+        //         name,
+        //         infoHash,
+        //         magnetURI,
+        //         torrentFileBlobURL,
+        //         timeRemaining,
+        //         downloaded,
+        //         uploaded,
+        //         downloadSpeed,
+        //         uploadSpeed,
+        //         progress,
+        //         numPeers,
+        //         files: results
+        //     }
+        // })
+
         // console.log(this.currentTorrent);
     }
     seedTorrentUpdate(infoHash, torrent) {
@@ -95,6 +149,32 @@ export default class TorrentStore {
             uploaded,
             uploadSpeed,
             numPeers,
+        }
+    }
+    addTorrentFileUpdate(fileHash, file) {
+        // console.log(file.downloaded);
+
+        let findIndex = this.currentTorrentFiles.findIndex((file) => file.key === fileHash);
+        const {
+            done,
+            downloaded,
+            progress
+        } = file;
+        this.currentTorrentFiles[findIndex] = {
+            ...this.currentTorrentFiles[findIndex],
+            done,
+            downloaded,
+            progress,
+        }
+        if (done) {
+            clearInterval(this.updateQueue[fileHash]);
+            this.currentTorrentFiles[findIndex].progress = 1;
+            file.getBlob((err, blob) => {
+                this.currentTorrentFiles[findIndex].blob = blob;
+            })
+            file.getBlobURL((err, url) => {
+                this.currentTorrentFiles[findIndex].url = url;
+            })
         }
     }
     // Seed Actions
@@ -149,8 +229,8 @@ export default class TorrentStore {
 
             const { infoHash, magnetURI, torrentFileBlobURL, timeRemaining, downloaded, uploaded, downloadSpeed, uploadSpeed, progress, numPeers } = torrent;
             this.currentTorrent = {
+                ...this.currentTorrent,
                 isProgress: true,
-                finished: false,
                 infoHash,
                 magnetURI,
                 torrentFileBlobURL,
@@ -162,20 +242,67 @@ export default class TorrentStore {
                 progress,
                 numPeers
             }
-            let addTorrent = setInterval(() => { this.torrentUpdate(torrent) }, 500);
+            this.updateQueue[infoHash] = setInterval(() => { this.torrentUpdate(torrent) }, 500);
+
+            torrent.files.map(file => {
+                let fileHash = shortid.generate();
+                let data = {
+                    done: file.done,
+                    key: fileHash,
+                    name: file.name,
+                    length: file.length,
+                }
+                this.currentTorrentFiles.push(data);
+                this.updateQueue[fileHash] = setInterval(() => { this.addTorrentFileUpdate(fileHash, file) }, 500)
+            })
+            // map(torrent.files, (file, callback) => {
+            //     let data = {
+            //         done: file.done,
+            //         key: shortid.generate(),
+            //         name: file.name,
+            //         length: file.length,
+            //     }
+            //     file.getBlob((err, blob) => {
+            //         data.blob = blob;
+            //         file.getBlobURL((err, url) => {
+            //             data.url = url;
+            //             callback(null, data);
+            //         })
+            //     })
+            // }, (err, results) => {
+            //     // if (err) return console.log(err);
+            //     // console.log(results);
+            //     // this.currentTorrent.files = results;
+            // })
+            // this.updateQueue[infoHash] = setInterval(() => { this.torrentUpdate(torrent) }, 500);
+
+            // this.updateQueue[infoHash] = setInterval(() => { this.addTorrentFileUpdate(torrent) }, 500);
             torrent.on('done', () => {
                 console.log('done!');
-                clearInterval(addTorrent);
                 this.currentTorrent = {
                     ...this.currentTorrent,
                     isProgress: false,
-                    finished: true
+                    finished: true,
                 }
-                console.log(this.currentTorrent);
-                //     torrent.files.forEach(file => {
-                //         // do something with file
-                //         file.appendTo('body');
+                // map(torrent.files, (file, callback) => {
+                //     let data = {
+                //         done: file.done,
+                //         key: shortid.generate(),
+                //         name: file.name,
+                //         length: file.length,
+                //     }
+                //     file.getBlob((err, blob) => {
+                //         data.blob = blob;
+                //         file.getBlobURL((err, url) => {
+                //             data.url = url;
+                //             callback(null, data);
+                //         })
                 //     })
+                // }, (err, results) => {
+                //     if (err) return console.log(err);
+                //     // console.log(results);
+                //     this.currentTorrent.files = results;
+                // })
             });
         })
     }
